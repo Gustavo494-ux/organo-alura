9a3e5f8cd0355992d7b62846ec8204d22b8774d4
Gustavo Gama <gustavogama494@gmail.com>
2024-01-09 17:53:10 -0300

    modulo 2

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -6,7 +6,7 @@ const CampoTexto = (props) => {
     return (        
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input Placeholder={PlaceholderModificado}/>
+            <input placeholder={PlaceholderModificado} required={props.obrigatorio}/>
         </div>
     )
 }
--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,14 +1,34 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Salvando')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" Placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" Placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" Placeholder="Digite o endereço da imagem" />
+                <CampoTexto obrigatorio={true} label="Nome" Placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={false} label="Imagem" Placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} itens={times} label="Time"/>
+                <Botao >
+                    Criar Botao
+                </Botao>
             </form>
         </section>
     )
