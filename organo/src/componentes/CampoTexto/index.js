6dc12103167b163f53e7182329cc5907c35bae22
Gustavo Gama <gustavogama494@gmail.com>
2024-01-11 15:20:35 -0300

    modulo 3

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,12 +1,16 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
-
     const PlaceholderModificado = `${props.Placeholder}...`
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (        
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={PlaceholderModificado} required={props.obrigatorio}/>
+            <input  value={props.valor} onChange={aoDigitado}  placeholder={PlaceholderModificado} required={props.obrigatorio}/>
         </div>
     )
 }
--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -2,8 +2,10 @@ import './Formulario.css'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import Botao from '../Botao'
+import React, { useState } from 'react';
 
-const Formulario = () => {
+
+const Formulario = (props) => {
     const times = [
         'Programação',
         'Front-end',
@@ -14,18 +16,57 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Salvando')
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+//        props.aoColaborador({
+//            nome,
+//            cargo,
+//            imagem, time
+//        })
     }
 
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto obrigatorio={true} label="Nome" Placeholder="Digite seu nome" />
-                <CampoTexto obrigatorio={false} label="Imagem" Placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa obrigatorio={true} itens={times} label="Time"/>
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    Placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    Placeholder="Digite seu Cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={false}
+                    label="Imagem"
+                    Placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    itens={times}
+                    label="Time"
+                    valor={time}
+                    aoAlterado={valor => setTime(valor) }
+                />
                 <Botao >
                     Criar Botao
                 </Botao>
